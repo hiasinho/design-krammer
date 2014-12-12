9714c6222d9b96b5bc9bf31e1b63302d668e788b
Mathias Schneider <hiasinho@me.com>
2014-12-12 08:53:16 +0100

    Automated commit at 2014-12-12 07:53:15 UTC by middleman-deploy 0.3.0

---
+++ assets/javascripts/vendor/parallax.js
@@ -0,0 +1,30 @@
+(function() {
+  var parallax;
+
+  parallax = function(plxWindow) {
+    var plxBackground, plxBackgroundTopToPageTop, plxBackgroundTopToWindowBottom, plxBackgroundTopToWindowTop, plxSpeed, plxWindowTopToPageTop, plxWindowTopToWindowTop, windowInnerHeight, windowTopToPageTop;
+    plxBackground = plxWindow.find(".parallax-background");
+    plxWindowTopToPageTop = $(plxWindow).offset().top;
+    windowTopToPageTop = $(window).scrollTop();
+    plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+    plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+    windowInnerHeight = window.innerHeight;
+    plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+    plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+    plxSpeed = 0.35;
+    return plxBackground.css("top", -(plxWindowTopToWindowTop * plxSpeed) + "px");
+  };
+
+  $(document).ready(function() {
+    return $('.parallax-window').each(function() {
+      return parallax($(this));
+    });
+  });
+
+  $(window).scroll(function(e) {
+    return $('.parallax-window').each(function() {
+      return parallax($(this));
+    });
+  });
+
+}).call(this);
